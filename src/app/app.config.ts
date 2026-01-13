fbf04586cafddeb7a5264f216f4c54e1e27f8d4d
KleberMera <klebermera2016@gmail.com>
2026-01-12 23:04:53 -0500

    feat: Implementación de la funcionalidad de modo oscuro y gestión del estado de la aplicación
    - Se añadió la interfaz AppState para definir la estructura del estado de la aplicación.
    - Se creó la configuración de AppTheme para temas claros y oscuros con PrimeUI.
    - Se introdujo AppConfigService para gestionar el estado de la aplicación, incluyendo la alternancia de temas y la visibilidad del menú.
    - Se actualizó el componente de la aplicación para utilizar señales para la gestión del estado y la alternancia de temas.
    - Se añadió un script de inicialización en index.html para aplicar la configuración del tema almacenada al cargar.
    - Se mejoraron los estilos globales para incluir Tailwind CSS y PrimeIcons.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
-
+import MyTheme from './core/types/apptheme';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    // providePrimeNG({
+    //   theme: {
+    //     preset: Aura,
+    //   },
+    // }),
+        providePrimeNG({ theme: MyTheme, ripple: true }),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { AppConfigService } from './shared/services/appconfigservice';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule, RouterOutlet, NgClass],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('nuevoamanecer');
+
+  checked1 = signal<boolean>(true);
+  value = signal<string>('');
+  showLoginDialog = signal<boolean>(false);
+
+  protected readonly configService = inject(AppConfigService);
+  ///private readonly router = inject(Router);
+
+  toggleDarkMode() {
+    this.configService.appState.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
+  }
+
+  isDarkMode = computed(() => this.configService.appState().darkTheme);
+
+  /* onLogin() {
+    this.showLoginDialog.set(true);
+    
+    // Simular proceso de login
+    setTimeout(() => {
+      this.showLoginDialog.set(false);
+      // Redirigir al dashboard después del login exitoso
+      this.router.navigate([PAGES_ROUTES.DASHBOARD.DASHBOARD]);
+    }, 2000);
+  }*/
 }
