bf64d6c713439fa7a43ef8a7983b7a53b898c232
KleberMera <klebermera2016@gmail.com>
2026-01-17 00:20:37 -0500

    refactor: Eliminar el modelo User y reemplazarlo por el modelo Usuario en el servicio de autenticación

---
+++ src/app/core/models/usuario.ts
@@ -0,0 +1,14 @@
+interface Usuario {
+  id: number;
+  nombre1: string;
+  nombre2?: string | null;
+  apellido1: string;
+  apellido2?: string | null;
+  telefono: string;
+  nombreUsuario: string;
+  email?: string | null;
+  password: string;
+  cedula?: string | null;
+  rolId: number;
+  estado?: string | null;
+}
--- src/app/features/auth/pages/services/auth.ts
+++ src/app/features/auth/pages/services/auth.ts
@@ -3,7 +3,6 @@ import { inject, Injectable, signal } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Storage } from '../../../../shared/services/storage';
-import { User } from '../../../../core/models/user';
 
 @Injectable({
   providedIn: 'root',
@@ -14,10 +13,10 @@ export class Auth {
   private keyUser = signal<string>('user');
   private keyToken = signal<string>('access_token');
 
-  formlogin(data: Partial<User> = {}) {
+  formlogin(data: Partial<Usuario> = {}) {
     const form = signal<FormGroup>(
       new FormGroup({
-        username: new FormControl(data.username, [Validators.required]),
+        nombreUsuario: new FormControl(data.nombreUsuario, [Validators.required]),
         password: new FormControl(data.password, [
           Validators.required,
           Validators.minLength(8),
