30a0d421e6ca063d204c7c9c342ea2cdf9bfd314
KleberMera <klebermera2016@gmail.com>
2026-01-21 17:09:24 -0500

    feat: Actualizar URL de API en el entorno y comentar la URL anterior

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl : 'https://1sow8dpavl.execute-api.us-west-1.amazonaws.com/production/api',
-    
+    //apiUrl : 'https://1sow8dpavl.execute-api.us-west-1.amazonaws.com/production/api',
+    apirUrl : 'https://nuevoamanecer-apirest.onrender.com/api',    
 };
