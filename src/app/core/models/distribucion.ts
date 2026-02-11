dbfe53c7d6506eeae247bf9fc0f20dffb2bd68a4
KleberMera <klebermera2016@gmail.com>
2026-02-10 22:45:07 -0500

    feat: Añadir interfaz de DistribucionPeriodo y mejorar la página de distribución por periodo con lógica de selección de año y manejo de errores

---
+++ src/app/core/models/distribucion.ts
@@ -0,0 +1,11 @@
+export interface DistribucionPeriodo {
+  periodo: string;
+  mes: string;
+  totalcobrado: number;
+  totalinteres: number;
+  totalcapital: number;
+  interesporciento: number;
+  interesmenosporciento: number;
+  totalacciones: number;
+  utilidadporaccion: number;
+}
--- src/app/features/nomina/pages/dist-periodo/dist-periodo.ts
+++ src/app/features/nomina/pages/dist-periodo/dist-periodo.ts
@@ -1,34 +1,59 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
 import { httpResource } from '@angular/common/http';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { NominaService } from '../../services/nomina-service';
+import { apiResponse } from '@app/core/models/apiResponse';
+import { DistribucionPeriodo } from '@app/core/models/distribucion';
 
 @Component({
   selector: 'app-dist-periodo',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    SelectModule,
+    ProgressSpinnerModule,
+    CurrencyPipe,
+    DecimalPipe,
+  ],
   templateUrl: './dist-periodo.html',
   styleUrl: './dist-periodo.css',
 })
 export default class DistPeriodo {
   private readonly nominaService = inject(NominaService);
-  private readonly yearSeleccionado = signal<string>(
-    new Date().getFullYear().toString()
-  );
+  private readonly currentYear = new Date().getFullYear();
+  private readonly yearSeleccionado = signal<string>(this.currentYear.toString());
 
-  protected distribucion = httpResource<unknown>(() =>
+  protected yearControl = new FormControl<string>(this.currentYear.toString());
+  protected yearsList = computed(() => [
+    { label: this.currentYear.toString(), value: this.currentYear.toString() },
+    {
+      label: (this.currentYear - 1).toString(),
+      value: (this.currentYear - 1).toString(),
+    },
+    {
+      label: (this.currentYear - 2).toString(),
+      value: (this.currentYear - 2).toString(),
+    },
+  ]);
+
+  protected distribucion = httpResource<apiResponse<DistribucionPeriodo[]>>(() =>
     this.nominaService.getDistribucionPagos(this.yearSeleccionado())
   );
 
-  constructor() {
-    effect(() => {
-      const data = this.distribucion.value();
-      if (data !== undefined) {
-        console.log('Distribucion pagos', data);
-      }
+  protected distribucionList = computed(() =>
+    this.distribucion.value()?.data ?? []
+  );
+  protected isLoading = computed(() => this.distribucion.isLoading());
+  protected hasError = computed(() => !!this.distribucion.error());
+  protected errorMessage = computed(() =>
+    this.distribucion.error() ? 'No se pudo cargar la distribucion' : ''
+  );
 
-      const error = this.distribucion.error();
-      if (error) {
-        console.error('Error distribucion pagos', error);
-      }
+  constructor() {
+    this.yearControl.valueChanges.subscribe((value) => {
+      this.yearSeleccionado.set(value || this.currentYear.toString());
     });
   }
 }
